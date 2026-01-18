8e0c051d47e9ff1a538e38fa9b5faa0f93e237d8
Syed Farhad <139438042+FarhadAgha@users.noreply.github.com>
2026-01-18 10:56:57 +0500

    Refactor pnds.py to improve readability and structure

--- pnds.py
+++ pnds.py
@@ -54,30 +54,6 @@ print("\ninner Join:\n", inner_merge)
 '''             //              '''
 
 import pandas as pd
-import matplotlib.pyplot as plt
-data = {'city':['Qta', 'Lahore', 'Karachi'],
-        'Sales':[250, 400, 350]}
-df = pd.DataFrame(data)
-
-plt.figure()
-df.plot(x='city', y='Sales', kind='line', title='Line Plot')
-plt.show()
-
-plt.figure()
-df.plot(x='city', y='Sales', kind='bar', title='Bar Plot')
-plt.show()
-
-plt.figure()
-df.plot(y='Sales', kind='pie', labels=df['city'], autopct='%1.1f%%', legend=False)
-plt.show()
-
-plt.figure()
-df.plot(kind='box', title='Box Plot')
-plt.show()
-
-'''               //                      '''
-
-import pandas as pd
 Data = pd.DataFrame({'Name': ['Ali', 'Ayesha', 'Usman', 'Sara'], 
                      'Age': [15, 16, 15, 14],
                      'Grade': ['A', 'B', 'A', 'C']})
@@ -114,4 +90,5 @@ df_sales = pd.DataFrame({'Sale ID': [101, 102, 103],
 marged = pd.merge(df_products, df_sales, on='Product ID')
 marged['TotalValue'] = marged['Price'] * marged['Quantity']
 print(df_products)
+
 print(marged)
